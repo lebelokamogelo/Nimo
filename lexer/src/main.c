9a7cd0565e6266013a32ef89756a8bbf647c5a00
lebelokamogelo <lebelokamogelo47@gmail.com>
2025-05-22 03:18:16 +0200

    generate ast from sources

--- lexer/src/main.c
+++ lexer/src/main.c
@@ -5,29 +5,15 @@
 #include "./lexer.c"
 #include "./parser.c"
 
-ASTNode *parse_1_plus_2(Token *tokens);
-int ast_current_index = 0;
-
-// print the AST in prefix notation
-void print_ast(ASTNode *node)
-{
-    if (node == NULL)
-        return;
-    printf("%s\n", node->value);
-    print_ast(node->left);
-    print_ast(node->right);
-}
-
 int main(void)
 {
-    char *source = "let x = 1 + 2;";
+    char *source = "let x = 1 + 2";
 
     List *list = Tokens(source);
 
-    Token tokens[5];
-
     Parser parser;
     parser.tokens = list->data;
+    parser.size = list->size;
     parser.current = 0;
 
     parse_statement(&parser);
@@ -35,48 +21,5 @@ int main(void)
     free(parser.tokens);
     printf("Completed\n");
 
-    tokens[0] = (Token){.type=TOKEN_NUMBER, .value="1"};
-    tokens[1] = (Token){.type=TOKEN_PLUS, .value="+"};
-    tokens[2] = (Token){.type=TOKEN_NUMBER, .value="2"};
-    tokens[3] = (Token){.type=TOKEN_PLUS, .value="+"};
-    tokens[4] = (Token){.type=TOKEN_NUMBER, .value="3"};
-    
-    // Writing a simple parser
-    ASTNode *ast = parse_1_plus_2(tokens);
-
-    print_ast(ast);
-    
-
     return 0;
 }
-
-ASTNode *create_ast_op(char *operator, ASTNode *left, ASTNode *right)
-{
-    ASTNode *node = (ASTNode *)malloc(sizeof(ASTNode));
-    node->type = AST_BINARY;
-    node->value = operator;
-    node->left = left;
-    node->right = right;
-    return node;
-}
-
-ASTNode *parse_1_plus_2(Token *tokens)
-{
-    ASTNode *node = (ASTNode *)malloc(sizeof(ASTNode));
-    node->type = AST_NUMBER;
-    node->value = tokens[ast_current_index].value;
-    node->left = NULL;
-    node->right = NULL;
-    ast_current_index++;
-
-    while (ast_current_index < 5 && tokens[ast_current_index].type == TOKEN_PLUS)
-    {
-        char *operator = tokens[ast_current_index].value;
-        ast_current_index++;
-        ASTNode *right = parse_1_plus_2(tokens);
-        
-        node = create_ast_op(operator, node, right);
-    }
-    return node;
-}
-
--- lexer/src/parser.c
+++ lexer/src/parser.c
@@ -3,6 +3,10 @@
 
 Token current_token(Parser *parser)
 {
+    if (parser->current >= parser->size)
+    {
+        return (Token){.type = TOKEN_EOF, .value = NULL};
+    }
     return parser->tokens[parser->current];
 }
 
@@ -25,14 +29,46 @@ void expect(Parser *parser, TokenType type)
     }
 }
 
-void parse_term(Parser *parser)
+ASTNode *create_ast_op(char *operator, ASTNode * left, ASTNode *right)
 {
-    Token token = current_token(parser);
-    if (token.type == TOKEN_NUMBER || token.type == TOKEN_IDENTIFIER)
+    ASTNode *node = (ASTNode *)malloc(sizeof(ASTNode));
+    node->type = TOKEN_PLUS;
+    node->value = operator;
+    node->left = left;
+    node->right = right;
+    return node;
+}
+
+ASTNode *create_number_node(Parser *parser)
+{
+    ASTNode *node = (ASTNode *)malloc(sizeof(ASTNode));
+    node->type = TOKEN_NUMBER;
+    node->value = current_token(parser).value;
+    node->left = NULL;
+    node->right = NULL;
+
+    return node;
+}
+
+ASTNode *parse_term(Parser *parser)
+{
+
+    ASTNode *node = create_number_node(parser);
+    printf("%s\n", node->value);
+
+    if (node->type == TOKEN_NUMBER || node->type == TOKEN_IDENTIFIER)
     {
         advance(parser);
+        Token node_bin = current_token(parser);
+        if (node_bin.value == NULL)
+            return node;
+
+        else if (node_bin.type == TOKEN_SEMICOLON)
+            return node;
+
+        printf("%s\n", node_bin.value);
     }
-    else if (token.type == TOKEN_LPAREN)
+    else if (node->type == TOKEN_LPAREN)
     {
         advance(parser);
         parse_expression(parser);
@@ -40,14 +76,16 @@ void parse_term(Parser *parser)
     }
     else
     {
-        fprintf(stderr, "Error: Expected NUMBER, IDENTIFIER, or LPAREN, got %d\n", token.type);
+        fprintf(stderr, "Error: Expected NUMBER, IDENTIFIER, or LPAREN, got %d\n", node->type);
         exit(1);
     }
+
+    return node;
 }
 
-void parse_expression(Parser *parser)
+ASTNode *parse_expression(Parser *parser)
 {
-    parse_term(parser);
+    ASTNode *node = parse_term(parser);
     Token token = current_token(parser);
     while (token.type == TOKEN_PLUS)
     {
@@ -55,6 +93,7 @@ void parse_expression(Parser *parser)
         parse_term(parser);
         token = current_token(parser);
     }
+    return node;
 }
 
 void parse_statement(Parser *parser)
@@ -63,5 +102,5 @@ void parse_statement(Parser *parser)
     expect(parser, TOKEN_IDENTIFIER);
     expect(parser, TOKEN_EQUALS);
     parse_expression(parser);
-    expect(parser, TOKEN_SEMICOLON);
+    // expect(parser, TOKEN_SEMICOLON);
 }
